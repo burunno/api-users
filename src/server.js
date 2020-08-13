71f50a66949cc1d33cad6b6c51c47754cc922c2a
Bruno Carvalho <brunocarvalho@MacBook-Air-de-Bruno.local>
2020-08-13 20:14:49 -0300

    server listening

--- src/server.js
+++ src/server.js
@@ -18,4 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes)
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening');
+});
